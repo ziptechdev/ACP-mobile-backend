5a4a26a77b68011b835162a573dec301cf9997d2
Mahix <mahir_b@msn.com>
2022-10-21 19:49:14 +0200

    User and Bank Account migrations and models

---
+++ src/models/User.ts
@@ -0,0 +1,47 @@
+import { JSONSchema, Model, RelationMappings } from 'objection';
+import BankAccount from './BankAccount';
+
+export default class User extends Model {
+  static get tableName(): string {
+    return 'users';
+  }
+
+  static get jsonSchema(): JSONSchema {
+    return {
+      type: 'object',
+      required: ['first_name', 'last_name', 'social_security_number'],
+
+      properties: {
+        id: { type: 'integer' },
+        kyc_status: { type: ['integer', 'null'] },
+        nv_id: { type: ['string', 'null'] },
+        bank_account_id: { type: ['integer', 'null'] },
+        first_name: { type: 'string' },
+        last_name: { type: 'string' },
+        middle_name: { type: ['string', 'null'] },
+        email: { type: ['string', 'null'] },
+        password: { type: ['string', 'null'] },
+        zip_code: { type: ['string', 'null'] },
+        social_security_number: { type: 'integer' },
+        day_of_birth: { type: ['string', 'null'] },
+        street_number_and_name: { type: ['string', 'null'] },
+        city: { type: ['string', 'null'] },
+        state: { type: ['string', 'null'] },
+        phone_number: { type: ['integer', 'null'] },
+      },
+    };
+  }
+
+  static get relationMappings(): RelationMappings {
+    return {
+      bank_account: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: BankAccount,
+        join: {
+          from: 'users.bank_account_id',
+          to: 'bank_accounts.id',
+        },
+      },
+    };
+  }
+}
