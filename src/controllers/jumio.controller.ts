1aeb296073162df0b1ef99502aa8f3650f7c091e
Mahix <mahir_b@msn.com>
2023-01-03 22:14:07 +0100

    Clean jumio controller

--- src/controllers/jumio.controller.ts
+++ src/controllers/jumio.controller.ts
@@ -1,18 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
 import {
-  getUserJumioVerificationProcess,
-  getVerificationProcessStatus,
   jumioProcessCallback,
   startIndentityVerification,
 } from '../services/api/jumio.service';
 import { httpResponse } from '../utils/httpResponse';
 import httpStatus from 'http-status';
-import { sendEmail } from '../mailer';
-import { fromEmailAddress } from '../config/vars';
-import { EmailVerificationParams } from '../shared/types/userTypes/params';
 import { JumioCallbackParameters } from '../shared/types/jumoTypes/jumioCallbackParametersTypes';
-import JumioVerificationProcesses from '../models/JumioVerificationProcesses';
-import { VerificationProcessStatus } from '../shared/types/jumoTypes/verificationProcessStatus';
 
 export const ResidentIdentityVerification = async (
   req: Request,
